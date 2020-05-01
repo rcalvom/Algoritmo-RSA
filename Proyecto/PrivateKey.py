b9f9cc975ec52c9e0580ae5f6caf8ce1598e361b
Ricardo Andrés Calvo <rcalvom@unal.edu.co>
2020-04-30 22:59:09 -0500

    Avance notebook.

--- Proyecto/PrivateKey.py
+++ Proyecto/PrivateKey.py
@@ -1,8 +1,5 @@
 
 class PrivateKey:
-    """def __int__(self):
-        self.module = 0
-        self.exponent = 0"""
 
     def __init__(self, module, exponent):
         self.module = module
--- Proyecto/PublicKey.py
+++ Proyecto/PublicKey.py
@@ -1,8 +1,5 @@
 
 class PublicKey:
-    """def __int__(self):
-        self.module = 0
-        self.exponent = 0"""
 
     def __init__(self, module, exponent):
         self.module = module
